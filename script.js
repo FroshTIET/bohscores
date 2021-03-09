01685fd03bfb6dc8e641564cb62cbd9f8ba13221
Rishabh <rishabhrare8@gmail.com>
2021-03-09 17:25:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ ourRequest.onload = function(){
         else if(ourData.clans[i].name === "theLeagueOfAssassins") score4.textContent = ourData.clans[i].score;
     }
     for(var i=0; i<ourData.events.length; i++){
-        totevent.innerHTML += "<a target=”_blank” href=" +ourData.events[i].link + ">" + <h2>ourData.events[i].name</h2> + "</a>"
+        totevent.innerHTML += "<a target=”_blank” href=" +ourData.events[i].link + ">" + <h2>ourData.events[i].name</h2> + "</a>";
         console.log(ourData.events[i].name);
     }
 };
